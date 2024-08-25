1b89dc2538d0128e64fc36adefc5463e8f0ab06d
vishal18713 <lcs2023052@iiitl.ac.in>
2024-08-26 04:02:27 +0530

    chore: Update dependencies and remove unused code

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,6 +1,9 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contract with the account:", deployer.address);
     
     const baseURI = "https://apricot-adorable-buzzard-685.mypinata.cloud/ipfs/QmafYVRMa9aWj2QZACYeUfbDXttryS5SydazdijXcNVfms/";
     const MyNFTCollection = await ethers.deployContract("MyNFTCollection", [baseURI]);
---
+++ src/frontend/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
+function App() {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask installed!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Connect to MetaMask</h1>
+      {account ? (
+        <p>Connected Account: {account}</p>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
